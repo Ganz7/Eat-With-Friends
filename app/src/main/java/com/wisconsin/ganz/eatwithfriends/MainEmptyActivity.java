f483434ceb9f312b32cb5093212d6f0fc28a8484
Ganz7 <ganzse7en@gmail.com>
2016-03-26 19:44:07 -0500

    Created empty activity with no UI

--- app/src/main/java/com/wisconsin/ganz/eatwithfriends/MainEmptyActivity.java
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/MainEmptyActivity.java
@@ -7,6 +7,15 @@ public class MainEmptyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        /* Logic here to check if user is already logged in */
+
+        // If user is not logged in
+        // Log them in or register them
+
+        // Else
+        // skip the login/register activity
     }
 }
