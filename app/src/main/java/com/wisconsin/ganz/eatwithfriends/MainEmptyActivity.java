e2b58da1e3cfff1909fee5f53ec854895cb6db9b
Ganz7 <ganzse7en@gmail.com>
2016-03-26 19:38:21 -0500

    Initial Commit

---
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/MainEmptyActivity.java
@@ -0,0 +1,12 @@
+package com.wisconsin.ganz.eatwithfriends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
