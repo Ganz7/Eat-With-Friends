b3bf3e66684b1ae3b8d4b4a24904bc1879d2d8a5
Ganz7 <ganzse7en@gmail.com>
2016-04-10 16:38:46 -0500

    Added HTTP Get Request to authenticate

--- app/src/main/java/com/wisconsin/ganz/eatwithfriends/LoginActivity.java
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/LoginActivity.java
@@ -28,7 +28,15 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,10 +47,10 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
+    // Id to identity READ_CONTACTS permission request.
     private static final int REQUEST_READ_CONTACTS = 0;
+    // Remote back-end URI
+    public static final String HOST_NAME = "https://wisc-eatwithfriends.herokuapp.com";
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -310,23 +318,41 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
+                StringBuilder urlBuilder = new StringBuilder(HOST_NAME);
+                urlBuilder.append("/authentication?user_email=");
+                urlBuilder.append(mEmail);
+                urlBuilder.append("&user_password=");
+                urlBuilder.append(mPassword);
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+                Toast.makeText(getApplicationContext(), "URL: "+urlBuilder.toString(), Toast.LENGTH_LONG).show();
+                URL url = new URL(urlBuilder.toString());
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-length", "0");
+                urlConnection.setUseCaches(false);
+                urlConnection.setConnectTimeout(6000);
+                urlConnection.setReadTimeout(6000);
+                urlConnection.connect();
+
+                if (urlConnection.getResponseCode() == 200 || urlConnection.getResponseCode() == 201) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader
+                            (urlConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    br.close();
+                    return true;
                 }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
             // TODO: register the new account here.
             return true;
         }
@@ -337,8 +363,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
                 finish();
-
                 // Call Main Activity from here
 
             } else {
