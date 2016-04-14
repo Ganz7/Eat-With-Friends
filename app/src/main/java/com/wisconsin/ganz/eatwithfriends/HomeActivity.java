88aebbd3870945b7e5f1f439880496a30ec2e92d
Ganz7 <ganzse7en@gmail.com>
2016-04-14 08:32:14 -0500

    Added stuff to create event activity

--- app/src/main/java/com/wisconsin/ganz/eatwithfriends/HomeActivity.java
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/HomeActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
@@ -28,11 +27,12 @@ public class HomeActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent addEvent = new Intent(HomeActivity.this, CreateEvent.class);
+                startActivity(addEvent);
             }
         });
 
