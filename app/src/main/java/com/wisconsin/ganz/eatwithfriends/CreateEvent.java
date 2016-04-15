ad5236d3cde841f700161f27848e80643d0d5f32
Ganz7 <ganzse7en@gmail.com>
2016-04-14 20:36:42 -0500

    Process Input Functionality

--- app/src/main/java/com/wisconsin/ganz/eatwithfriends/CreateEvent.java
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/CreateEvent.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class CreateEvent extends AppCompatActivity {
 
@@ -32,7 +37,7 @@ public class CreateEvent extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                processSubmit();
+                extractDataAndSubmit();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -53,6 +58,8 @@ public class CreateEvent extends AppCompatActivity {
 
         setTimePickerListenerHelper(et_start_time);
         setTimePickerListenerHelper(et_end_time);
+
+        EditTextDatePicker startDatePicker = new EditTextDatePicker(this, R.id.et_date);
 
     }
 
@@ -86,7 +93,50 @@ public class CreateEvent extends AppCompatActivity {
     /**
      * Process user request to create a new event
      */
-    private void processSubmit(){
+    private void extractDataAndSubmit(){
+        String location = et_location.getText().toString().trim();
+        String date = et_date.getText().toString().trim();
+        String start_time = et_start_time.getText().toString().trim();
+        String end_time = et_end_time.getText().toString().trim();
+        String info = et_info.getText().toString().trim();
+
+        // Check if the fields are filled in
+        // TODO: Make toast notifications individually
+        if(location.length() == 0 || date.length() == 0 || start_time.length() == 0 || end_time.length() == 0){
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String startDateString = et_date + " " + et_start_time;
+        String endDateString = et_date + " " + et_end_time;
+
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm"); //Custom time format
+
+        Date startDate = null;
+        Date endDate = null;
+        try {
+            startDate = format.parse(startDateString);
+            endDate = format.parse(endDateString);
+        } catch (ParseException e) {
+            Log.e("Parse Exception", "Error : "+e.getMessage());
+        }
+
+        long startMillis = startDate.getTime();
+        long endMillis = endDate.getTime();
+
+        //If the event is in reverse chronological order, do not add
+        if(startMillis > endMillis){
+            Toast.makeText(this, "Start time cannot be later than end time", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        addEvent(location, date, start_time, end_time, info);
+    }
+
+    /**
+     *
+     */
+    private void addEvent(String location, String date, String start_time, String end_time, String info){
 
     }
 
