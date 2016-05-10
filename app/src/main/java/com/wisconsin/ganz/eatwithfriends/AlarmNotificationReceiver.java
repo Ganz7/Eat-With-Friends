cf068e6240dde36ec01488dde22e9bc756979912
Ganz7 <ganzse7en@gmail.com>
2016-05-10 00:24:19 -0500

    Added BroadcastReceiver

---
+++ app/src/main/java/com/wisconsin/ganz/eatwithfriends/AlarmNotificationReceiver.java
@@ -0,0 +1,13 @@
+package com.wisconsin.ganz.eatwithfriends;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmNotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
